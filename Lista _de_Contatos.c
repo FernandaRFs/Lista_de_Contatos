aefcc353cdec586ff6a9b13e945ebc12c2401067
Armando <113314643+Armandoedu@users.noreply.github.com>
2023-09-28 21:16:32 -0300

    Update Lista _de_Contatos.c

--- Lista _de_Contatos.c
+++ Lista _de_Contatos.c
@@ -1,1 +1,19 @@
+#include "ListadeContatos.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
+#define TAM 31
+
+
+void inicializar(Contatos tabela[]){
+    int i;
+    for ( i = 0; i < TAM; i++)
+    {
+        tabela[i].tell = 0;
+    }
+}
+
+int hash(int chave){
+    return chave%TAM;
+}
